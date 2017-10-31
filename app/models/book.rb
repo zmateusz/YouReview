dac29406264ff82ab04b16395386bf2d2900fd51
Mateusz Zdanowicz <zmateusz@users.noreply.github.com>
2017-10-31 18:41:16 +0100

    Add image for missing Book cover

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,6 @@ class Book < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :reviews
-  has_attached_file :book_img, styles: { book_index: "300x300>", book_show: "325x475>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :book_img, styles: { book_index: "300x300>", book_show: "325x475>" }, default_url: "/images/missing.png"
   validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
 end
